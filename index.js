45daf211856a00a97d50df64486d6d5f30fbf1c6
GO3LIN <benmoha.mehdi@gmail.com>
2019-07-05 17:04:42 +0200

    feat(server): Wire delete endpoint and set json as output format

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const PORT = 2222;
 
 const playersApi = require('./players');
 
+const playerRegex = /\/players\/([0-9]+)$/;
+
 const handleRequest = (req, res) => {
 
+  res.setHeader('Content-Type', 'application/json');
+
   const route = url.parse(req.url).pathname;
-  const playerRegex = /\/players\/([0-9]+)/;
 
   // LIST players endpoint
   if(route === '/players' && req.method === 'GET') {
@@ -17,14 +20,19 @@ const handleRequest = (req, res) => {
   // GET player endpoint
   else if(playerRegex.test(route) && req.method === 'GET') {
     // Get player id from url
-    const playerId = parseInt(route.match(playerRegex)[1], 10);
+    const playerId = getPlayerId(route);
     const player = playersApi.getPlayer(playerId);
     res.end(JSON.stringify(player));
   }
   // DELETE player endpoint
-  else if(route === '/players' && req.method === 'DELETE') {
-    
+  else if(playerRegex.test(route) && req.method === 'DELETE') {
+    console.log('delete');
+    const playerId = getPlayerId(route);
+    const players = playersApi.deletePlayer(playerId);
+    res.end(JSON.stringify(players));
   }
+
+  res.end(JSON.stringify('Hello world !'));
 }
 
 const server = http.createServer(handleRequest);
@@ -32,4 +40,8 @@ const server = http.createServer(handleRequest);
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 })
+
+getPlayerId = route => {
+  return parseInt(route.match(playerRegex)[1], 10)
+};
 
