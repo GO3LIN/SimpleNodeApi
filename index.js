5da2730c3aeec44936b007239827a0dece6346aa
GO3LIN <benmoha.mehdi@gmail.com>
2019-07-05 16:32:49 +0200

    feat(players): Get a player endpoint implementation

--- index.js
+++ index.js
@@ -7,23 +7,27 @@ const playersApi = require('./players');
 const handleRequest = (req, res) => {
 
   const route = url.parse(req.url).pathname;
+  const playerRegex = /\/players\/([0-9]+)/;
 
-  // LIST players
+  // LIST players endpoint
   if(route === '/players' && req.method === 'GET') {
-    const players = productsApi.listPlayers();
+    const players = playersApi.listPlayers();
     res.end(JSON.stringify(players));
   }
-  // GET player
-  else if(/\/players\/[0-9]+/.test(route) && req.method === 'GET') {
-
+  // GET player endpoint
+  else if(playerRegex.test(route) && req.method === 'GET') {
+    // Get player id from url
+    const playerId = parseInt(route.match(playerRegex)[1], 10);
+    const player = playersApi.getPlayer(playerId);
+    res.end(JSON.stringify(player));
   }
-  // DELETE player
+  // DELETE player endpoint
   else if(route === '/players' && req.method === 'DELETE') {
     
   }
 }
 
-const server = http.createServer();
+const server = http.createServer(handleRequest);
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
