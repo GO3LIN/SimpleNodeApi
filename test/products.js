2e4e9cbf961a3ad8333e367770f1915cab010094
GO3LIN <benmoha.mehdi@gmail.com>
2019-07-05 15:10:23 +0200

    test(players): Init tests

---
+++ test/products.js
@@ -0,0 +1,21 @@
+const expect =  require('chai').expect;
+
+describe('players api', function(){
+
+  it('should list players', function(){
+    
+  });
+
+  it('should add player', function(){
+
+  });
+
+  it('should get player', function(){
+
+  });
+
+  it('should delete player', function(){
+
+  });
+
+})
